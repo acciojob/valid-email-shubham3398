d732d5aa2f613e9d9a9573f9a0d8fdc70df38efd
shubham3398 <117896201+shubham3398@users.noreply.github.com>
2023-12-22 17:33:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 function validEmail(str) {
-  //your JS code here.
+  // Check for the presence of domain extensions
+  if (!str.includes(".com") && !str.includes(".co.in") && !str.includes(".edu")) {
+    return false;
+  }
+
+  // Check for the presence of '@'
+  if (!str.includes("@")) {
+    return false;
+  }
+
+  // Find the index of '@'
+  let atIndex = str.indexOf('@');
+
+  // Ensure that there are characters before and after '@'
+  if (atIndex === 0 || atIndex === str.length - 1) {
+    return false;
+  }
+
+  // Ensure that there is a dot (.) after '@'
+  if (!str.includes(".", atIndex)) {
+    return false;
+  }
+
+  return true;
 }
 
 // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
+
